6ed2ecbe4afefaebf6123203ea4dc3faf2b299aa
a <a@as-Virtual-Machine.local>
2023-11-22 20:53:17 -0800

    WIP

--- js/quick-start_onnxruntime-web-bundler/main.js
+++ js/quick-start_onnxruntime-web-bundler/main.js
@@ -3,84 +3,98 @@
 
 // see also advanced usage of importing ONNX Runtime Web:
 // https://github.com/microsoft/onnxruntime-inference-examples/tree/main/js/importing_onnxruntime-web
-const ort = require('onnxruntime-web');
+// const ort = require('onnxruntime-web');
 
-async function blobToFloat32Array(blob) {
-    return new Promise((resolve, reject) => {
-        let fileReader = new FileReader();
-        fileReader.onload = event => {
-            let arrayBuffer = event.target.result;
-            let audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            audioContext.decodeAudioData(arrayBuffer, buffer => {
-                let float32Array = buffer.getChannelData(0); // get data for the first channel
-                resolve(float32Array);
-            }, reject);
-        };
-        fileReader.onerror = reject;
-        fileReader.readAsArrayBuffer(blob);
-    });
-}
+// async function blobToFloat32Array(blob) {
+//     return new Promise((resolve, reject) => {
+//         let fileReader = new FileReader();
+//         fileReader.onload = event => {
+//             let arrayBuffer = event.target.result;
+//             let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//             audioContext.decodeAudioData(arrayBuffer, buffer => {
+//                 let float32Array = buffer.getChannelData(0); // get data for the first channel
+//                 resolve(float32Array);
+//             }, reject);
+//         };
+//         fileReader.onerror = reject;
+//         fileReader.readAsArrayBuffer(blob);
+//     });
+// }
 
-// use an async context to call onnxruntime functions.
-async function main(data) {
-    try {
-        // create a new session and load the specific model.
-        //
-        const session = await ort.InferenceSession.create('./silero_vad.onnx');
+// // use an async context to call onnxruntime functions.
+// async function main(data) {
+//     try {
+//         // create a new session and load the specific model.
+//         //
+//         const session = await ort.InferenceSession.create('./silero_vad.onnx');
 
         
-        let audioFrame = await blobToFloat32Array(data);
+//         let audioFrame = await blobToFloat32Array(data);
         
-        const t = new ort.Tensor("float32", audioFrame, [1, audioFrame.length])
-        const zeroes = Array(2 * 64).fill(0)
-        const h = new ort.Tensor("float32", zeroes, [2, 1, 64]);
-        const c = new ort.Tensor("float32", zeroes, [2, 1, 64]);
-        const sr = new ort.Tensor("int64", [16000n])
-        // prepare inputs. a tensor need its corresponding TypedArray as data
-        // feed inputs and run
-        //
-        const inputs = {
-          input: t,
-          h: h,
-          c: c,
-          sr: sr,
-        }
-        const results = await session.run(inputs);
-        console.log(results);
+//         const t = new ort.Tensor("float32", audioFrame, [1, audioFrame.length])
+//         const zeroes = Array(2 * 64).fill(0)
+//         const h = new ort.Tensor("float32", zeroes, [2, 1, 64]);
+//         const c = new ort.Tensor("float32", zeroes, [2, 1, 64]);
+//         const sr = new ort.Tensor("int64", [16000n])
+//         // prepare inputs. a tensor need its corresponding TypedArray as data
+//         // feed inputs and run
+//         //
+//         const inputs = {
+//           input: t,
+//           h: h,
+//           c: c,
+//           sr: sr,
+//         }
+//         const results = await session.run(inputs);
+//         console.log(results);
 
-        // read from results
-        // const dataC = results.c.data;
-        // document.write(`data of result tensor 'c': ${dataC}`);
+//         // read from results
+//         // const dataC = results.c.data;
+//         // document.write(`data of result tensor 'c': ${dataC}`);
 
-    } catch (e) {
-        document.write(`failed to inference ONNX model: ${e}.`);
-    }
-}
+//     } catch (e) {
+//         document.write(`failed to inference ONNX model: ${e}.`);
+//     }
+// }
 
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(function(stream) {
-    const mediaRecorder = new MediaRecorder(stream);
-    let audioChunks = [];
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//   .then(function(stream) {
+//     const mediaRecorder = new MediaRecorder(stream);
+//     let audioChunks = [];
 
-    mediaRecorder.start();
+//     mediaRecorder.start();
 
-    mediaRecorder.addEventListener("dataavailable", function(event) {
-      audioChunks.push(event.data);
-      main(event.data);
-    });
+//     mediaRecorder.addEventListener("dataavailable", function(event) {
+//       audioChunks.push(event.data);
+//       main(event.data);
+//     });
 
-    mediaRecorder.addEventListener("stop", function() {
-      console.log("stop");
-      const audioBlob = new Blob(audioChunks);
-      const audioUrl = URL.createObjectURL(audioBlob);
-      const audio = new Audio(audioUrl);
-      audio.play();
-    });
+//     mediaRecorder.addEventListener("stop", function() {
+//       console.log("stop");
+//       const audioBlob = new Blob(audioChunks);
+//       const audioUrl = URL.createObjectURL(audioBlob);
+//       const audio = new Audio(audioUrl);
+//       audio.play();
+//     });
 
-    setTimeout(() => {
-      mediaRecorder.stop();
-    }, 1000);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+//     setTimeout(() => {
+//       mediaRecorder.stop();
+//     }, 1000);
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//   });
+
+// main.js
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+async function init() {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  await audioContext.audioWorklet.addModule('vad-processor.js');
+  const vadNode = new AudioWorkletNode(audioContext, 'vad-processor');
+  const source = audioContext.createMediaStreamSource(stream);
+  source.connect(vadNode);
+  vadNode.connect(audioContext.destination);
+};
+
+init();
---
+++ js/quick-start_onnxruntime-web-bundler/vad-processor.js
@@ -0,0 +1,12 @@
+// voice-activity-detection-processor.js
+class VoiceActivityDetectionProcessor extends AudioWorkletProcessor {
+  process(inputs, outputs, parameters) {
+    // Your voice activity detection logic goes here.
+    // inputs is an array of input audio data.
+    // outputs is an array where you can push your output audio data.
+    // parameters is an object for parameter data.
+    return true; // keep the processor alive
+  }
+}
+
+registerProcessor('vad-processor', VoiceActivityDetectionProcessor);
